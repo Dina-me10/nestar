67b0776f857f9f1ff1008828936dbd7161850591
MIRISKAKOVAMIRJALILKIZI MADINA <dinaisakova10@gmail.com>
2026-09-03 00:23:34 +0900

    feat: create follow related resolver and service model

--- apps/nestar-api/src/components/components.module.ts
+++ apps/nestar-api/src/components/components.module.ts
@@ -10,12 +10,13 @@ import { BoardArticleModule } from './board-article/board-article.module';
 
 @Module({
   imports: [MemberModule,
-     PropertyModule,
       AuthModule, 
-      CommentModule,
+      PropertyModule,
+      BoardArticleModule,
        LikeModule,
         ViewModule,
+        CommentModule,
          FollowModule,
-          BoardArticleModule]
+          ]
 })
 export class ComponentsModule {}
